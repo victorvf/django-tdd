f6e9068535a2d75d4bc757e222bdec8e67762924
victorvf <victorvitoria11@hotmail.com>
2020-11-10 22:23:16 -0300

    feature: now the view can receive a post request

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item',
         )
 
-        # Ela digita "Buy peacock feathers em uma caixa de texto.
+        # Ela digita "Buy peacock feathers" em uma caixa de texto.
         inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
@@ -40,22 +40,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a
         # acrescentar outro item.
         # Ela insere 'Use peacock feathers to make a fly'
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em
         # sua lista
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith fica na dúvida se o site lembrará de sua lista. Então ela
         # nota que o site gerou um URL único para ela -- há um pequeno
         # texto explicativo para isso.
+        self.fail('Finish the test!')
 
         # Ela acessa essa URL - sua lista de tarefas continua lá.
 
