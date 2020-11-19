eac44b28cdcecb9116b4873dd733c454395ef8c2
victorvf <victorvitoria11@hotmail.com>
2020-11-18 22:42:24 -0300

    feature: new URL, view and template to display lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, view_list
 
 urlpatterns = [
     path("", home_page, name="home"),
+    path("lists/the-only-list-in-the-world/", view_list, name="view_list")
 ]
