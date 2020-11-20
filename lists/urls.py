a12b30fb769ef302be1ac9f3abcc339118e89168
victorvf <victorvitoria11@hotmail.com>
2020-11-20 00:27:22 -0300

    feature: new URL

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home_page, view_list
+from .views import home_page, view_list, new_list
 
 urlpatterns = [
     path("", home_page, name="home"),
-    path("lists/the-only-list-in-the-world/", view_list, name="view_list")
+    path("lists/the-only-list-in-the-world/", view_list, name="view_list"),
+    path('lists/new/', new_list, name='new_list'),
 ]
