d832e249d03beaac169ddf98bf1b71e2b6a6f1b2
victorvf <victorvitoria11@hotmail.com>
2020-11-23 22:24:36 -0300

    feature(lists): new URL + view for adding to existing lists. FT passes

--- lists/urls.py
+++ lists/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_page, view_list, new_list
+from .views import home_page, view_list, new_list, add_item
 
 urlpatterns = [
     path("", home_page, name="home"),
-    path("lists/the-only-list-in-the-world/", view_list, name="view_list"),
+    path("lists/<int:list_id>/", view_list, name="view_list"),
     path('lists/new/', new_list, name='new_list'),
+    path("lists/<int:list_id>/add_item/", add_item, name="add_item"),
 ]
