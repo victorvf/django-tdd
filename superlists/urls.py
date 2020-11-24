2ed239acdf08aa7259b598a33b29bed17bb77dfe
victorvf <victorvitoria11@hotmail.com>
2020-11-23 22:26:28 -0300

    refactor: urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from lists.views import home_page
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("lists.urls")),
+    path("", home_page, name="home"),
+    path("lists/", include("lists.urls")),
 ]
